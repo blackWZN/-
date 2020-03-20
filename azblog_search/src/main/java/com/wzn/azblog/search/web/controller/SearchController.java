4d5a18938abba9666a5538bcce6bb3dda186ada5
znwei <297130962@qq.com>
2020-03-20 16:43:39 +0800

    更新

--- azblog_search/src/main/java/com/wzn/azblog/search/web/controller/SearchController.java
+++ azblog_search/src/main/java/com/wzn/azblog/search/web/controller/SearchController.java
@@ -5,18 +5,21 @@ import com.wzn.azblog.search.entity.EsArticle;
 import com.wzn.azblog.search.entity.EsCategory;
 import com.wzn.azblog.search.entity.EsComment;
 import com.wzn.azblog.search.service.SearchService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/search")
+@Api(tags = {"搜索模块"})
 public class SearchController {
 
     @Autowired
     private SearchService articleService;
 
-    //文章搜索
+    @ApiOperation(value = "文章搜索")
     @GetMapping("/{keywords}/{page}/{limit}")
     public PageResult search(@PathVariable("keywords") String keywords, @PathVariable Integer page, @PathVariable Integer limit){
 
@@ -25,7 +28,7 @@ public class SearchController {
                 pageInfo.getTotalPages(),pageInfo.getContent());
     }
 
-    //分类搜索搜索
+    @ApiOperation(value = "分类搜索")
     @GetMapping("/categroy/{keywords}/{page}/{limit}")
     public PageResult searchCategroy(@PathVariable("keywords") String keywords, @PathVariable Integer page, @PathVariable Integer limit){
         Page<EsCategory> pageInfo = articleService.searchCategroy(keywords, page, limit);
@@ -33,7 +36,7 @@ public class SearchController {
                 pageInfo.getTotalPages(),pageInfo.getContent());
     }
 
-    //评论搜索搜索
+    @ApiOperation(value = "评论搜索")
     @GetMapping("/comment/{keywords}/{page}/{limit}")
     public PageResult searchComment(@PathVariable("keywords") String keywords, @PathVariable Integer page, @PathVariable Integer limit){
         Page<EsComment> pageInfo = articleService.searchComment(keywords, page, limit);
