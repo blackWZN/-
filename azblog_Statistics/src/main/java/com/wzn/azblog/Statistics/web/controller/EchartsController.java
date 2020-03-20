4382d5291cda0c76a443da7d060306cb888d0395
znwei <297130962@qq.com>
2020-03-20 16:44:12 +0800

    增加swagger文档

--- azblog_Statistics/src/main/java/com/wzn/azblog/Statistics/web/controller/EchartsController.java
+++ azblog_Statistics/src/main/java/com/wzn/azblog/Statistics/web/controller/EchartsController.java
@@ -4,6 +4,8 @@ import com.wzn.ablog.common.entity.EchartsDimensions;
 import com.wzn.ablog.common.vo.AzResult;
 import com.wzn.ablog.common.vo.EchartsResult;
 import com.wzn.azblog.Statistics.service.EchartsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/echarts")
+@Api(tags = {"数据统计"})
 public class EchartsController {
 
     @Autowired
@@ -22,12 +25,14 @@ public class EchartsController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    @ApiOperation(value = "图表数据")
     @GetMapping("/dataset")
     public AzResult dimensions(){
         EchartsResult list = echartsService.list();
         return AzResult.ok().data(list);
     }
 
+    @ApiOperation(value = "在线人数")
     @GetMapping("/onlineCount")
     public AzResult onlineCount(){
         Object onlineCount = redisTemplate.opsForValue().get("onlineCount");
